0b8c4e981e994468007f071d4bbd52a70eeb88fb
kwabena koranteng <kwabena.obeng@ashesi.edu.gh>
2021-04-09 11:18:39 +0000

    sign up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,13 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import {HTTP } from '@ionic-native/http/ngx';
 import { FormsModule } from '@angular/forms';
+import {Toast} from '@ionic-native/toast/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,FormsModule ],
-  providers: [HTTP,{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [Toast,HTTP,{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
