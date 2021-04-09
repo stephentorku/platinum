9eded84ad728d53cb77bfb69a6ec9f2e3e84c013
kwabena koranteng <kwabena.obeng@ashesi.edu.gh>
2021-04-09 21:39:37 +0000

    Login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import{HTTP } from '@ionic-native/http/ngx';
+import {Toast} from '@ionic-native/toast/ngx';
+import {AlertController, NavController} from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  user:any={};
+  constructor(private http:HTTP, private toast: Toast , public navCtrl:NavController) { }
 
   ngOnInit() {
+  }
+  login(){
+    this.http.post('https://platinumhostel.000webhostapp.com/logintenant.php',{
+      email:this.user.email,
+      pass:this.user.pass
+
+    },{}).then(data=>{
+      console.log(data)
+    });
+    console.log(this.user);
+    // this.navCtrl.pop().then(()=>{
+    //   this.toast.show('New Member added','3000','bottom').subscribe(
+    //     toast =>{
+    //       console.log(toast);
+    //     }
+    //   )
+    //  });
   }
 
 }
