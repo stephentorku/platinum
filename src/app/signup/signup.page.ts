b9cc2b92eca52dbc0a49d862c654087cb77a2ece
kwabena koranteng <kwabena.obeng@ashesi.edu.gh>
2021-04-09 03:48:12 +0000

    Sign up done

--- src/app/signup/signup.page.ts
+++ src/app/signup/signup.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import{HTTP } from '@ionic-native/http/ngx';
+import {Toast} from '@ionic-native/toast/ngx';
+import {AlertController, NavController} from '@ionic/angular';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupPage implements OnInit {
 
-  constructor() { }
+  user:any={};
+  constructor(private http:HTTP, private toast: Toast , public navCtrl:NavController) { }
 
   ngOnInit() {
+  }
+
+
+  submit(){
+    this.http.post('https://platinumhostel.000webhostapp.com/addnewtenant.php',{
+      fname:this.user.fname , 
+      lname:this.user.lname,
+      gen:this.user.gen,
+      email:this.user.email,
+      number:this.user.number,
+      pass:this.user.pass
+
+    },{});
+    this.navCtrl.pop().then(()=>{
+      this.toast.show('New Member added','3000','bottom').subscribe(
+        toast =>{
+          console.log(toast);
+        }
+      )
+     });
   }
 
 }
