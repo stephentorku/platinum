3d2fbde965941a0119ef107e72bde3722a8994b6
Stephen Torku <stephantorku18@gmail.com>
2021-04-14 10:04:24 +0000

    loading

--- src/app/choose-room/choose-room.page.ts
+++ src/app/choose-room/choose-room.page.ts
@@ -15,17 +15,24 @@ export class ChooseRoomPage implements OnInit {
   responseData: any;
   responseObject: any={};
 
-  constructor(private router: Router,private http: HTTP, private roomservice: RoomService) { }
+  constructor(private router: Router,private http: HTTP, private roomservice: RoomService, public loadingController: LoadingController) { }
 
   ngOnInit() {
   }
 
   async getRoomByName(){
-    await this.http.post('https://platinumhostel.000webhostapp.com/search.php',
+
+    const loading = await this.loadingController.create({
+      message: 'Please wait...',
+    });
+
+    loading.present().then(async ()=>{
+      await this.http.post('https://platinumhostel.000webhostapp.com/search.php',
     {firstname:this.details.fname, lastname:this.details.lname},{}).then(data=>{
       console.log(data.data);
       this.responseData = JSON.parse(data.data);
     });
+
     console.log(this.responseData);
     this.roomservice.setRoomNumber(this.responseData.Name);
     this.roomservice.setCapacity(this.responseData.Capacity);
@@ -33,25 +40,41 @@ export class ChooseRoomPage implements OnInit {
     this.roomservice.setCost(this.responseData.cost);
     this.roomservice.setID(this.responseData.id);
     console.log(this.responseData.id);
+    loading.dismiss();
+
     if(this.roomservice.setRoomNumber(this.responseData.Name)){
       this.router.navigate(['/room-details']);
     }
+
+    });
+
+
+
+    
 
 
 
   }
   async getbycapacity(){
-    await this.http.post('https://platinumhostel.000webhostapp.com/searchbycapacity.php',
+
+    const loading = await this.loadingController.create({
+      message: 'Please wait...',
+    });
+
+    loading.present().then(async ()=>{
+      await this.http.post('https://platinumhostel.000webhostapp.com/searchbycapacity.php',
     {capacity:this.need.capacity},{}).then(data=>{
       console.log(data.data);
       this.responseObject = JSON.parse(data.data);
     });
 
     this.roomservice.setObject(this.responseData);
-
+    loading.dismiss();
     if(this.roomservice.setObject(this.responseObject)){
       this.router.navigate(['/all-room-details']);
     } 
+    });
+    
     
 
   }
--- src/app/signup/signup.page.ts
+++ src/app/signup/signup.page.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import{HTTP } from '@ionic-native/http/ngx';
 import {Toast} from '@ionic-native/toast/ngx';
 import {AlertController, NavController} from '@ionic/angular';
-
+import { LoadingController } from '@ionic/angular';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.page.html',
@@ -11,29 +12,49 @@ import {AlertController, NavController} from '@ionic/angular';
 export class SignupPage implements OnInit {
 
   user:any={};
-  constructor(private http:HTTP, private toast: Toast , public navCtrl:NavController) { }
+  constructor(private http:HTTP, private toast: Toast ,
+     public navCtrl:NavController, public loadingController: LoadingController,
+     private router: Router, private alertctrl:AlertController) { }
 
   ngOnInit() {
   }
 
 
-  submit(){
-    this.http.post('https://platinumhostel.000webhostapp.com/addnewtenant.php',{
-      fname:this.user.fname , 
-      lname:this.user.lname,
-      gen:this.user.gen,
-      email:this.user.email,
-      number:this.user.number,
-      pass:this.user.pass
+  async submit(){
+    const loading = await this.loadingController.create({
+      message: 'Please wait...',
+    });
 
-    },{});
-    this.navCtrl.pop().then(()=>{
-      this.toast.show('New Member added','3000','bottom').subscribe(
-        toast =>{
-          console.log(toast);
-        }
-      )
-     });
+    loading.present().then(async ()=>{
+      await this.http.post('https://platinumhostel.000webhostapp.com/addnewtenant.php',{
+        fname:this.user.fname , 
+        lname:this.user.lname,
+        gen:this.user.gen,
+        email:this.user.email,
+        number:this.user.number,
+        pass:this.user.pass
+  
+      },{});
+      loading.dismiss();
+
+      await this.alertctrl.create({
+        header:'Success!',
+        subHeader:'Signed Up successfully',
+        buttons:[{
+          text:"Okay",
+          handler:()=>{
+            this.router.navigate(['/login']).then(() => {
+              window.location.reload();
+            });
+          }
+        }]
+      }).then((confirmElement)=>{
+        confirmElement.present()
+      })
+    });
+
+    
+    
   }
 
 }
